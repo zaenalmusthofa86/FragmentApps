8cb41350dd24885e0fdfbeb25c2def5dde5920dc
Zaenal Mustofa <zaenalmusthofa86>
2021-04-11 11:31:29 +0700

    FragmentApps First Commit

---
+++ app/src/main/java/com/zaenal/fragmenappszaenal/FirstFragment.java
@@ -0,0 +1,6 @@
+package com.zaenal.fragmenappszaenal;
+
+import android.app.Fragment;
+
+public class FirstFragment extends Fragment {
+}
---
+++ app/src/main/java/com/zaenal/fragmenappszaenal/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.zaenal.fragmenappszaenal;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}
